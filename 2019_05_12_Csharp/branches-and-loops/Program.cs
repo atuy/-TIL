fd08a430784ca0482532f16e65f9832a014ef747
찬명 박 <pcm0028@naver.com>
2019-05-12 18:46:00 +0900

    Add 2019/05/12 learned c#

---
+++ 2019_05_12_Csharp/branches-and-loops/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace branches_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+                Console.WriteLine();
+            }
+
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            /*
+             * if는 bool의 값을 확인하고 참인 경우 if 뒤의 문장을 실행합니다
+             * 그렇지 않으면 건너뜁니다
+             * else키워드 뒤의 문은 테스트 하는 조건이 거짓을 경우 실행된다
+             * 들여쓰기가 중요하지; 않기 때문에 {}을 사용하여 
+             * 두 개 이상의 문장을이 블록의 일부가 되는 시기를 나타내야 한다
+             * 
+             * == 기호는 같음을 테스트 한다 =과 ==가 구분 된다
+             * &&는 and를 나타낸다 참 분기에서 문장을 실행 하려면 두 조건이 모두 참이여야 한다
+             * || 를 사용하여 or를 나타 낼 수 있다
+             * 
+             */
+
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+            Console.WriteLine();
+            /*
+             * while문은 조건을 확인하고 뒤의 문장을 실행 한다 조건이 거짓이 될 떄까지 조건확인 및 문장 실행을 반복한다
+             * ++ 는 증가 연산자 +1을 더하고 값을 저장한다
+             */
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+            Console.WriteLine();
+            /*
+             *애-while 문은 while 뒤에 코드를 먼저 실행 한 후에 조건을 확인합니다  
+             */
+
+            for (counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+            }
+            Console.WriteLine();
+            /*
+             * for 문에는 작동 방식을 제어하는 세부분이 있다
+             * 첫번째 부분은 for 이니셜라이저이다 counter이 루프 변수이고 첫 값을 0으로 설정한다
+             * 중간 부분은 for 조건이다 counter < 10은 for 루프가 카운터 값이 10보다 작으면 계속 실행됨을 선언합니다
+             * 마지막 부분은 for 반복기이다 counter++은 for문 다음의 블록을 실행 한 후 루프 변수를 수정하는 방법을 지정한다
+             */
+
+            int sum = 0;
+            for(int i=1;i<=20;i++)
+            {
+                if(i%3==0)
+                {
+                    Console.Write($"{i}\t");
+                    sum += i;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
--- C#/HelloWorld/numbers-in-Csharp/Program.cs
+++ C#/HelloWorld/numbers-in-Csharp/Program.cs
@@ -6,7 +6,97 @@ namespace numbers_in_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine($"a={a}\t b={b}\t c={c}");
+            Console.WriteLine($"a+b*c={d}");
+            Console.WriteLine();
+
+            a = 7;
+            b = 4;
+            c = 3;
+            d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"a={a}\t b={b}\t c={c}");
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+            Console.WriteLine();
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+            Console.WriteLine();
+            /*
+             * c#의 정수 형식은 최소 한도와 최대 한도가 있다 
+             */
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+            Console.WriteLine();
+
+            /*
+             * double 방식은 배정밀도 부동 소수점을 나타낸다
+             * 부동 소수점은 아주 크거나 작은 정수가 아닌 수를 나타낼 때 유용합니다.
+             * 배정밀도란 이러한 숫자가 단정밀도보다 큰 전체 자리수를 사용하여 저장됨을 의미 합니다
+             * 컴퓨터에서는 배정밀도를 더 많이 사용함
+             */
+
+            double d_a = 5;
+            double d_b = 4;
+            double d_c = 2;
+            double d_d = (d_a + d_b) / d_c;
+            Console.WriteLine($"a={d_a}\tb={d_b}\tc={d_c}\n a + b / c = {d_d}");
+            Console.WriteLine();
+
+            d_a = 19;
+            d_b = 23;
+            d_c = 8;
+            d_d = (d_a + d_b) / d_c;
+            Console.WriteLine($"a={d_a}\tb={d_b}\tc={d_c}\n a + b / c = {d_d}");
+            Console.WriteLine();
+
+
+            double d_max = double.MaxValue;
+            double d_min = double.MinValue;
+            Console.WriteLine($"The range of double is {d_min} to {d_max}\n");
+            /*
+             * 최대 최소값은 과학적 표기법으로 출력된다
+             * E 의 왼쪽에 있는 숫자는 유효 숫자이고 오른쪽의 숫자는 지수이다
+             */
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+            /*
+             * 0.3333333과 1/3은 정확하게 동일하지 않다
+             */
+
+            decimal de_min = decimal.MinValue;
+            decimal de_max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {de_min} to {de_max}");
+            Console.WriteLine();
+
+            /*
+             *decimal 형식은 범위가 적지만 double 보다 전체 자리수가 더 크다
+             */
+
+            d_a = 1.0;
+            d_b = 3.0;
+            Console.WriteLine(d_a / d_b);
+            Console.WriteLine();
+
+            decimal de_c = 1.0M;
+            decimal de_d = 3.0M;
+            Console.WriteLine(de_c / de_d);
+            Console.WriteLine();
+            /*
+             * 숫자의 M 접미사는 상수가 decimal 형식을 사용해야합을 나타낸다
+             */
+
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+
         }
     }
 }
